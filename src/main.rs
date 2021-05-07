717310c36d8c7aeddf8549264a8ce0f25466dc58
Amanjeev Sethi <aj@amanjeev.com>
2021-05-07 16:53:46 -0400

    simd-search update with correct way to access the derive

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-#[macro_use]
-extern crate serde_derive;
-
-use simd_json;
-use simd_json_derive;
+use simd_json_derive::{Serialize, Deserialize};
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, simd_json_derive::Deserialize)]
 struct SIMDExample<'sin> {
     id: u64,
-    #[serde(borrow)]
     id_str: &'sin str,
 }
 
@@ -21,7 +16,8 @@ fn main() {
     for line in reader.lines() {
         //println!("{}", line.unwrap());
         let row: &mut str = &mut line.unwrap();
-        let row: SIMDExample = simd_json::serde::from_str(row).unwrap();
+        let row: SIMDExample = SIMDExample::from_str(row).unwrap();
+
         match row.id {
             2807149942735425369 => println!("look ma! a match! - {}", row.id_str),
             _ => println!("No match yet"),
